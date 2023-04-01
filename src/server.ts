c3ad7dac24a99929b5adf2f60777fc2e35df346f
Denisio Rodrigues <denisio@ymail.com>
2023-03-31 21:10:59 -0300

    realizando queries

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
+app.post('/hello', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transção de teste',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
+})
+
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transactions
 })
 
 app
