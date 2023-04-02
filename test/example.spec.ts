3b5ea20263dad0afa9872f681ed43b03d3e17304
Denisio Rodrigues <denisio@ymail.com>
2023-04-02 18:38:11 -0300

    Categorizando o testre

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,23 +1,25 @@
-import { afterAll, beforeAll, test } from 'vitest'
+import { afterAll, beforeAll, it, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-beforeAll(async () => {
-  await app.ready()
-})
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
 
-afterAll(async () => {
-  await app.close()
-})
+  afterAll(async () => {
+    await app.close()
+  })
 
-test('user can created a new tarnsaction', async () => {
-  // Fazer uma chamada HTTP para criar uma nova transação
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New Transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+  it('shoud be able to create a new trasnaction', async () => {
+    // Fazer uma chamada HTTP para criar uma nova transação
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
 })
